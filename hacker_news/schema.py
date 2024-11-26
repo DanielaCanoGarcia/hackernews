647034519ca8e87f9c3f05b0039906e682b99940
Daniela Cano Garcia <159411509+DanielaCanoGarcia@users.noreply.github.com>
2024-11-26 00:52:47 -0600

    update schema in hackernews

--- hacker_news/schema.py
+++ hacker_news/schema.py
@@ -3,11 +3,12 @@ import graphql_jwt
 
 import links.schema
 import users.schema
+import api_logs.schema
 
-class Query(users.schema.Query, links.schema.Query, graphene.ObjectType):
+class Query(api_logs.schema.Query, users.schema.Query, links.schema.Query, graphene.ObjectType):
     pass
 
-class Mutation(users.schema.Mutation, links.schema.Mutation, graphene.ObjectType):
+class Mutation(api_logs.schema.Mutation, users.schema.Mutation, links.schema.Mutation, graphene.ObjectType):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
